61e9c9fdee474f45daf7e67247229d4ac6546a59
NightmareFox <carloshenriquez392004@gmail.com>
2025-03-14 12:58:46 -0400

    fix bugs

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,3 +1,9 @@
+import { z } from 'zod';
+import { Toaster, toast } from 'sonner';
+
+import type { Route } from './+types';
+import { FaArrowLeft, FaEye, FaEyeSlash } from 'react-icons/fa6';
+import { useEffect, useState } from 'react';
 import { Label } from '@radix-ui/react-label';
 import { Link } from 'react-router';
 import { Button } from '~/components/ui/button';
@@ -9,104 +15,263 @@ import {
   CardTitle,
 } from '~/components/ui/card';
 import { Input } from '~/components/ui/input';
-import type { Route } from './+types';
-import { FaArrowLeft } from 'react-icons/fa6';
+import { AnimatePresence, motion } from 'motion/react';
+import {
+  emailSchema,
+  lastNameSchema,
+  nameSchema,
+  passwordSchema,
+} from '~/utils/schemas';
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'Registro' }];
 }
 
 const Register = () => {
+  //states
+  const [name, setName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const [nameError, setNameError] = useState<string>('');
+  const [lastNameError, setLastNameError] = useState<string>('');
+  const [emailError, setEmailError] = useState<string>('');
+  const [passwordError, setPasswordError] = useState<string>('');
+
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
   //functions
   const handleCreateUser = async () => {
     try {
-      console.log('pisao')
+      // nameSchema
       const req = await fetch('http://localhost:3000/create-user', {
         method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
         body: JSON.stringify({
-          name: 'juan',
-          jose: 'pedro',
+          name,
+          lastName,
+          email,
+          password,
         }),
       });
 
       const res = await req.json();
 
-      console.log(res);
+      if (res.message !== undefined)
+        toast.error(res.message, { richColors: true });
     } catch (err) {
       console.log(err);
     }
   };
 
+  //effects
+  useEffect(() => {
+    const result = nameSchema.safeParse(name);
+
+    if (!result.success) setNameError(result.error.errors[0].message);
+    else setNameError('');
+  }, [name]);
+
+  useEffect(() => {
+    const result = lastNameSchema.safeParse(lastName);
+
+    if (!result.success) setLastNameError(result.error.errors[0].message);
+    else setLastNameError('');
+  }, [lastName]);
+
+  useEffect(() => {
+    const result = lastNameSchema.safeParse(lastName);
+
+    if (!result.success) setLastNameError(result.error.errors[0].message);
+    else setLastNameError('');
+  }, [lastName]);
+
+  useEffect(() => {
+    const result = emailSchema.safeParse(email);
+
+    if (!result.success) setEmailError(result.error.errors[0].message);
+    else setEmailError('');
+  }, [email]);
+
+  useEffect(() => {
+    const result = passwordSchema.safeParse(password);
+
+    if (!result.success) setPasswordError(result.error.errors[0].message);
+    else setPasswordError('');
+  }, [password]);
+
   return (
-    <main className='flex flex-col items-center'>
-      <div className='relative flex w-full p-3'>
-        <Link to='/'>
-          <Button variant='ghost' className='rounded-full border size-12'>
-            <FaArrowLeft className='w-16' />
-          </Button>
-        </Link>
-      </div>
-
-      <Card className='w-96 sm:w-7/12 md:w-6/12 z-20'>
-        <CardHeader>
-          <CardTitle className='text-2xl'>Registro</CardTitle>
-          <CardDescription>Ingresa tu correo y contraseña</CardDescription>
-        </CardHeader>
-        <CardContent>
-          {/* Name */}
-          <div className='flex flex-col gap-6'>
-            <div className='grid gap-2'>
-              <Label htmlFor='name'>Nombres</Label>
-              <Input
-                id='name'
-                type='text'
-                placeholder='Jose'
-                required
-                autoComplete='off'
-              />
-            </div>
-            {/* LastName  */}
-            <div className='grid gap-2'>
-              <Label htmlFor='lastName'>Apellidos</Label>
-              <Input
-                id='lastName'
-                type='text'
-                placeholder='Rebolledo'
-                required
-                autoComplete='off'
-              />
-            </div>
-            {/* Email */}
-            <div className='grid gap-2'>
-              <Label htmlFor='email'>Correo Electrónico</Label>
-              <Input
-                id='email'
-                type='email'
-                placeholder='jose@example.com'
-                required
-              />
-            </div>
-
-            {/* Password  */}
-            <div className='grid gap-2'>
-              <div className='flex items-center'>
-                <Label htmlFor='password'>Contraseña</Label>
-              </div>
-              <Input
-                id='password'
-                placeholder='********'
-                type='password'
-                required
-              />
-            </div>
-
-            <Button className='w-full mt-5 bg-green-800 hover:bg-green-900 delay-75 transition-all' onClick={ handleCreateUser}>
-              Registrarse
+    <>
+      <Toaster position='bottom-center' />
+      <main className='flex flex-col items-center'>
+        <div className='relative flex w-full p-3'>
+          <Link to='/'>
+            <Button variant='ghost' className='rounded-full border size-12'>
+              <FaArrowLeft className='w-16' />
             </Button>
-          </div>
-        </CardContent>
-      </Card>
-    </main>
+          </Link>
+        </div>
+
+        <Card className='w-96 sm:w-7/12 md:w-6/12 lg:w-6/12 z-20 outline-1 shadow-xl p-5'>
+          <CardHeader>
+            <CardTitle className='text-2xl text-center'>Registro</CardTitle>
+            {/* <CardDescription>Ingresa tu correo y contraseña</CardDescription> */}
+          </CardHeader>
+          <CardContent>
+            <div className='flex flex-col gap-6'>
+              {/* Name */}
+              <div className='grid gap-2'>
+                <Label htmlFor='name'>
+                  Nombre <span className='text-red-400 font-bold'>*</span>
+                </Label>
+                <div>
+                  <Input
+                    id='name'
+                    type='text'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder='Jose'
+                    required
+                    autoComplete='off'
+                    autoCapitalize='words'
+                  />
+                  <AnimatePresence>
+                    {name.length > 0 && nameError !== '' && (
+                      <motion.p
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        className='font-bold text-sm text-red-500 ps-2 pt-1'
+                      >
+                        {nameError}
+                      </motion.p>
+                    )}
+                  </AnimatePresence>
+                </div>
+              </div>
+
+              {/* LastName  */}
+              <div className='grid gap-2'>
+                <Label htmlFor='lastName'>
+                  Apellido <span className='text-red-400 font-bold'>*</span>
+                </Label>
+                <div>
+                  <Input
+                    id='lastName'
+                    type='text'
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    placeholder='Rebolledo'
+                    required
+                    autoComplete='off'
+                    autoCapitalize='words'
+
+                  />
+                  <AnimatePresence>
+                    {lastName.length > 0 && lastNameError !== '' && (
+                      <motion.p
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        className='font-bold text-sm text-red-500 ps-2 pt-1'
+                      >
+                        {lastNameError}
+                      </motion.p>
+                    )}
+                  </AnimatePresence>
+                </div>
+              </div>
+
+              {/* Email */}
+              <div className='grid gap-2'>
+                <Label htmlFor='email'>
+                  Correo Electrónico
+                  <span className='text-red-400 font-bold'>*</span>
+                </Label>
+                <div>
+                  <Input
+                    id='email'
+                    type='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder='jose@example.com'
+                    required
+                  />
+                  <AnimatePresence>
+                    {email.length > 0 && emailError !== '' && (
+                      <motion.p
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        className='font-bold text-sm text-red-500 ps-2 pt-1'
+                      >
+                        {emailError}
+                      </motion.p>
+                    )}
+                  </AnimatePresence>
+                </div>
+              </div>
+
+              {/* Password  */}
+              <div className='grid gap-2'>
+                <div className='flex items-center'>
+                  <Label htmlFor='password'>
+                    Contraseña <span className='text-red-400 font-bold'>*</span>
+                  </Label>
+                </div>
+                <div>
+                  <div className='flex'>
+                    <Input
+                      id='password'
+                      type={showPassword ? 'text' : 'password'}
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      placeholder='********'
+                      required
+                    />
+                    <Button
+                      variant='ghost'
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </Button>
+                  </div>
+
+                  <AnimatePresence>
+                    {password.length > 0 && passwordError !== '' && (
+                      <motion.p
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        className='font-bold text-sm text-red-500 ps-2 pt-1'
+                      >
+                        {passwordError}
+                      </motion.p>
+                    )}
+                  </AnimatePresence>
+                </div>
+              </div>
+
+              <Button
+                className='w-full mt-5 bg-green-800 hover:bg-green-900 delay-75 transition-all'
+                onClick={handleCreateUser}
+                disabled={
+                  nameError !== '' ||
+                  lastNameError !== '' ||
+                  emailError !== '' ||
+                  passwordError !== ''
+                }
+              >
+                Registrarse
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </main>
+    </>
   );
 };
 
